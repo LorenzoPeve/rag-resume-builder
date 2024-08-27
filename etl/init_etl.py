bab97d7cddd837f440a6f6d0ec355cd715e4ebc9
Lorenzo Peve <lpeve01@gmail.com>
2024-08-26 23:40:29 -0500

    Update init_etl.py

--- etl/init_etl.py
+++ etl/init_etl.py
@@ -10,10 +10,11 @@ from src import embeddings
 load_dotenv(override=True)
 
 # Get embeddings
-with open(os.path.join(os.path.dirname(__file__), "shakespeare.txt")) as f:
+path = os.path.join(os.path.dirname(__file__), "sample_data.txt")
+with open(path, encoding='utf-8') as f:
     text = f.read()
 e = embeddings.Embeddings(text, chunk_size=500, chunk_overlap=50)
-vectors = e.get_embeddings()
+vectors = e.get_text_and_embeddings()
 
 # Connect to the database
 conn = psycopg.connect(
@@ -23,6 +24,9 @@ conn = psycopg.connect(
     user=os.getenv('DB_USER'),
     password=os.getenv('DB_PASSWORD'),
 )
+print(type(conn))
+cur = conn.cursor()
+print(type(cur))
 
 # Create tables and load records
 with conn as conn:
@@ -31,7 +35,7 @@ with conn as conn:
         DROP SCHEMA public CASCADE;
         CREATE SCHEMA public;
         
-        CREATE EXTENSION IF NOT EXISTS vector;
+        CREATE EXTENSION IF NOT EXISTS vector with SCHEMA public;
 
         CREATE TABLE records (
             id bigserial PRIMARY KEY,
@@ -39,6 +43,9 @@ with conn as conn:
             embedding vector(1536),
             search_text tsvector GENERATED ALWAYS AS (to_tsvector('english', text)) STORED
         );
+                
+        CREATE INDEX vector_hnsw_index ON records USING hnsw (embedding vector_cosine_ops);
+        SET maintenance_work_mem = '1GB';
     """)
     insert_query = """
     INSERT INTO records (text, embedding) VALUES (%s, %s)
